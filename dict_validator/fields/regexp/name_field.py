83cc4242f6b294325893284074a0e5ea1031d1bc
Anton Berezin <gurunars@gmail.com>
2016-07-10 21:00:33 +0300

    TODO

--- dict_validator/fields/regexp/name_field.py
+++ dict_validator/fields/regexp/name_field.py
@@ -6,6 +6,8 @@ from ..regexp_field import RegexpField
 class NameField(RegexpField):
     """
     Human name e.g. John Smith
+
+    TODO: add Unicode support
     """
 
     def __init__(self, *args, **kwargs):
