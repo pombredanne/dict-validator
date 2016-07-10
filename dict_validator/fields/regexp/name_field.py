0f2ab1a068b17eb3d0b6e3eac5337a1a81422bef
Anton Berezin <gurunars@gmail.com>
2016-07-10 21:24:12 +0300

    Fixed ASCII naming issues

--- dict_validator/fields/regexp/name_field.py
+++ dict_validator/fields/regexp/name_field.py
@@ -1,16 +1,28 @@
 """ dict_validator.fields.regexp.name_field """
 
+import re
+
 from ..regexp_field import RegexpField
 
 
 class NameField(RegexpField):
     """
     Human name e.g. John Smith
-
-    TODO: add Unicode support
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, lowercase_allowed=False, *args, **kwargs):
         super(NameField, self).__init__(
-            r"^[A-Z][a-z]+( [A-Z][a-z]+)*$",
+            r"^\w+( \w+)*$",
             "name", *args, **kwargs)
+        self._lowercase_allowed = lowercase_allowed
+
+    def _validate(self, value):
+        ret_val = super(NameField, self)._validate(value)
+        if ret_val:
+            return ret_val
+        if not self._lowercase_allowed:
+            for word in value.split():
+                if unicode(word[0]).islower():
+                    return "One of the name parts is lowercase"
+        if re.search(r"[0-9_]+", value):
+            return "Name can't contain digits"
--- test/fields/regexp/test_name_field.py
+++ test/fields/regexp/test_name_field.py
@@ -10,12 +10,17 @@ class NameFieldTest(unittest.TestCase):
             sorted(NameField().validate("Caleb Van Dorum")),
             [])
 
+    def test_nok_wrong_ascii_chars(self):
+        self.assertEqual(
+            sorted(NameField().validate("Caleb V@n Dorum")),
+            [([], 'Value "Caleb V@n Dorum" did not match name regexp')])
+
     def test_nok_lowercase(self):
         self.assertEqual(
             sorted(NameField().validate("Caleb van Dorum")),
-            [([], u'Value "Caleb van Dorum" did not match name regexp')])
+            [([], 'One of the name parts is lowercase')])
 
     def test_nok_numbers(self):
         self.assertEqual(
             sorted(NameField().validate("Caleb V01an Dorum")),
-            [([], u'Value "Caleb V01an Dorum" did not match name regexp')])
+            [([], "Name can't contain digits")])
