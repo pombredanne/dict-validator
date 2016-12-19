f782f1d059a381200d57e81dd420a8cdec8b8b0b
Anton Berezin <gurunars@gmail.com>
2016-12-19 14:23:20 +0200

    Number field min and max

--- dict_validator/fields/number_field.py
+++ dict_validator/fields/number_field.py
@@ -8,6 +8,22 @@ class NumberField(Field):
     Match integers, floats and longs
     """
 
+    def __init__(self, min=None, max=None, can_be_float=True):
+        self._min = min
+        self._max = max
+        self._can_be_float = can_be_float
+
     def _validate(self, value):
-        if not isinstance(value, (int, float, long)):
+        allowed_types = (int, long)
+        if self._can_be_float:
+            allowed_types += (float,)
+
+        if not isinstance(value, allowed_types):
             return "Not a number"
+
+    def _describe(self):
+        return {
+            "min": self._min,
+            "max": self._max,
+            "can_be_float": self._can_be_float
+        }
